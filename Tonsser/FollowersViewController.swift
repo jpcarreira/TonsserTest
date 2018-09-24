ff60759187745b8679c80dddf5c3e878179879c4
João Carreira <joaocarreira@me.com>
2018-09-24 01:42:17 +0100

    added table view with placeholder data

---
+++ Tonsser/FollowersViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FollowersViewController.swift
+//  Tonsser
+//
+//  Created by João Carreira on 24/09/2018.
+//  Copyright © 2018 jpcarreira. All rights reserved.
+//
+
+import UIKit
+
+
+final class FollowersViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.allowsSelection = false
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FollowerCell", for: indexPath)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 68
+    }
+}
