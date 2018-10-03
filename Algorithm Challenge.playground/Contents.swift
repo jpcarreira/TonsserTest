1315d0671ee79db54958aae42eb9d2d0b8b5cc15
João Carreira <joaocarreira@me.com>
2018-10-03 20:52:37 +0100

    algorithm challenge

--- Algorithm Challenge.playground/Contents.swift
+++ Algorithm Challenge.playground/Contents.swift
@@ -9,6 +9,12 @@ struct Match {
     let awayTeamName: String
 }
 
+extension Match: CustomStringConvertible {
+    var description: String {
+        return "\(homeTeamName) vs \(awayTeamName) at \(date)"
+    }
+}
+
 // Utility functions
 
 func createDate(from dateString: String) -> Date? {
@@ -19,11 +25,18 @@ func createDate(from dateString: String) -> Date? {
 
 // Return the closest match index to [dateTapped], return 0 otherwise, [matchList] is s​orted​ by date.
 func findClosestMatch(dateTapped: Date, matchList: [Match]) -> Int {
-    // TODO: implement...
+    let datesOnly = matchList.map { abs($0.date.timeIntervalSince(dateTapped)) }
+    if let closestDate = datesOnly.sorted().first(where: { $0 > 0 }) {
+        return datesOnly.index(of: closestDate) ?? 0
+    }
+    
     return 0
 }
 
-let date = Date()
+let now = Date()
+let date = createDate(from: "2018-10-27 19:00")
+let date2 = createDate(from: "2018-11-03 19:00")
+
 let matchList = [
     Match(slug: "game 1", date: createDate(from: "2018-10-02 20:00")!, homeTeamName: "AEK Athen", awayTeamName: "Benfica"),
     Match(slug: "game 2", date: createDate(from: "2018-10-07 17:30")!, homeTeamName: "Benfica", awayTeamName: "Porto"),
@@ -33,4 +46,7 @@ let matchList = [
     Match(slug: "game 6", date: createDate(from: "2018-11-07 20:00")!, homeTeamName: "Benfica", awayTeamName: "Ajax")
     ]
 
-print("Index of closest match: \(findClosestMatch(dateTapped: date, matchList: matchList))")
+
+print("Index of closest match to \(now): \(findClosestMatch(dateTapped: now, matchList: matchList)) \(matchList[findClosestMatch(dateTapped: now, matchList: matchList)])")
+print("Index of closest match to \(date!): \(findClosestMatch(dateTapped: date!, matchList: matchList)) \(matchList[findClosestMatch(dateTapped: date!, matchList: matchList)])")
+print("Index of closest match to \(date2!): \(findClosestMatch(dateTapped: date2!, matchList: matchList)) \(matchList[findClosestMatch(dateTapped: date2!, matchList: matchList)])")
